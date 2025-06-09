075220840e97acb8cc0722723b8c271b4b23554e
PEDRO HENRIQUE <mateusfernandes131004@gmail.com>
2025-06-08 21:04:47 -0300

    pão

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Digite o valor do salário-hora (R$)..............: ");
+double salarioHora = Convert.ToDouble(Console.ReadLine());
+ 
+Console.Write("Digite o total de horas trabalhadas...............: ");
+double horasTrabalhadas = Convert.ToDouble(Console.ReadLine());
+ 
+Console.Write("Digite o número de horas extras....................: ");
+double horasExtras = Convert.ToDouble(Console.ReadLine());
+ 
+double salarioFinal = CalcularSalario(salarioHora, horasTrabalhadas, horasExtras);
+ 
+Console.WriteLine();
+Console.WriteLine($"Salário total a receber: R$ {salarioFinal:F2}");
+ 
+double CalcularSalario(double valorHora, double horasNormais, double horasExtras)
+{
+    double salarioBase = valorHora * horasNormais;
+    double valorHoraExtra = valorHora * 1.4;
+    double salarioExtras = valorHoraExtra * horasExtras;
+ 
+    return salarioBase + salarioExtras;
+}
